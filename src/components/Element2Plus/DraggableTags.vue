d7780708fd41a198c22b4ed0611c59b21ca69b1b
lbc19920615 <1585638808@qq.com>
2021-02-23 14:04:33 +0800

    init

---
+++ src/components/Element2Plus/DraggableTags.vue
@@ -0,0 +1,49 @@
+<template>
+<draggable v-model="innerTags"
+           class="draggable-tags"
+@change="onChange"
+>
+  <el-tag
+      :key="tag"
+      v-for="tag in innerTags"
+      :disable-transitions="false">
+    {{tag}}
+  </el-tag>
+</draggable >
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+  name: 'DraggableTags',
+  props: {
+    tags: Array
+  },
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      innerTags: []
+    }
+  },
+  mounted() {
+    this.innerTags = this.tags
+  },
+  watch: {
+    tags(newVal) {
+      this.innerTags = newVal
+    }
+  },
+  methods: {
+    onChange() {
+      this.$emit('input', this.innerTags)
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
